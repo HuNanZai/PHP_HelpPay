5f2e32529f575b222b94b3bc577a2ffd3e0b94f5
HuNanZai <123165587@qq.com>
2015-10-26 15:49:33 +0800

    支付宝的支付类的编码风格统一成psr1/2

--- src/Service/Alipay/Config.php
+++ src/Service/Alipay/Config.php
@@ -16,18 +16,19 @@ class Config
     const SIGN_TYPE = 'MD5';            //签名方式 不需修改
     const INPUT_CHARSET = 'utf-8';      //字符编码格式 目前支持 gbk 或 utf-8
     const CACERT = '';                  //ca证书路径地址，用于curl中ssl校验 请保证cacert.pem文件在当前文件夹目录中
-    const TRANSPORT  = 'http';          //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
+    const TRANSPORT = 'http';          //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
 
-    public static function getConfigArray(){
+    public static function getConfigArray()
+    {
         return array(
-            'partner'   => self::PARTNER,
-            'seller_id' => self::SELLER_ID,
-            'private_key_path'  => self::PRIVATE_KEY_PATH,
-            'ali_public_key_path'   => self::PUBLIC_KEY_PATH,
-            'sign_type' => self::SIGN_TYPE,
-            'input_charset' => self::INPUT_CHARSET,
-            'cacert'        => self::CACERT,
-            'transport'     => self::TRANSPORT,
+            'partner'             => self::PARTNER,
+            'seller_id'           => self::SELLER_ID,
+            'private_key_path'    => self::PRIVATE_KEY_PATH,
+            'ali_public_key_path' => self::PUBLIC_KEY_PATH,
+            'sign_type'           => self::SIGN_TYPE,
+            'input_charset'       => self::INPUT_CHARSET,
+            'cacert'              => self::CACERT,
+            'transport'           => self::TRANSPORT,
         );
     }
 }
--- src/Service/Alipay/Service.php
+++ src/Service/Alipay/Service.php
@@ -19,27 +19,28 @@ class Service implements IThirdPartyPay
      * @param null $extra //订单的其他交易信息( title 订单标题, show_url 商品展示地址, body 订单描述等,有特殊需求的详见文档)
      *
      * @return mixed
-    */
+     */
     public function pay($trade_no, $amount, $extra = null)
     {
         $parameter = array(
-            'service'       => 'alipay.wap.create.direct.pay.by.user',
-            'partner'       => trim(Config::PARTNER),
-            'seller_id'     => trim(Config::SELLER_ID),
-            'payment_type'  => '1',
-            'notify_url'    => Config::NOTIFY_URL,
-            'return_url'    => Config::RETURN_URL,
-            'out_trade_no'  => $trade_no,
-            'subject'       => $extra['title'] ? $extra['title'] : '支付宝交易订单',
-            'total_fee'     => $amount,
-            'show_url'      => $extra['show_url'] ? $extra['show_url'] : '',
-            'body'          => $extra['body'] ? $extra['body'] : '',
-            'it_b_pay'      => $extra['it_b_pay'] ? $extra['it_b_pay'] : '',
-            'extern_token'  => $extra['extern_token'] ? $extra['extern_token'] : '',
-            '_input_charset'=> trim(Config::INPUT_CHARSET),
+            'service'        => 'alipay.wap.create.direct.pay.by.user',
+            'partner'        => trim(Config::PARTNER),
+            'seller_id'      => trim(Config::SELLER_ID),
+            'payment_type'   => '1',
+            'notify_url'     => Config::NOTIFY_URL,
+            'return_url'     => Config::RETURN_URL,
+            'out_trade_no'   => $trade_no,
+            'subject'        => $extra['title'] ? $extra['title'] : '支付宝交易订单',
+            'total_fee'      => $amount,
+            'show_url'       => $extra['show_url'] ? $extra['show_url'] : '',
+            'body'           => $extra['body'] ? $extra['body'] : '',
+            'it_b_pay'       => $extra['it_b_pay'] ? $extra['it_b_pay'] : '',
+            'extern_token'   => $extra['extern_token'] ? $extra['extern_token'] : '',
+            '_input_charset' => trim(Config::INPUT_CHARSET),
         );
 
         $submit = new Submit(Config::getConfigArray());
+
         return $submit->buildRequestForm($parameter, 'get', '确认');
     }
 
@@ -50,12 +51,13 @@ class Service implements IThirdPartyPay
      *
      * @return mixed
      */
-    public function notify($type=null)
+    public function notify($type = null)
     {
         $method = "notify{$type}";
-        if( method_exists($this, $method) ){
-            return call_user_func(array($this, $method) );
+        if (method_exists($this, $method)) {
+            return call_user_func(array($this, $method));
         }
+
         return false;
     }
 
@@ -64,18 +66,20 @@ class Service implements IThirdPartyPay
      *
      * @return array|bool
      */
-    public function notifyPayNotify(){
+    public function notifyPayNotify()
+    {
         $notify = new Notify(Config::getConfigArray());
         $result = $notify->verifyNotify();
-        if( !$result ){//验签失败 直接返回
+        if (!$result) {//验签失败 直接返回
             echo "fail";
+
             return false;
         }
         echo "success"; //验证成功就应该通知成功
         return array(
-            'out_trade_no'  => $_POST['out_trade_no'],
-            'trade_no'      => $_POST['trade_no'],
-            'trade_status'  => $_POST['trade_status'],
+            'out_trade_no' => $_POST['out_trade_no'],
+            'trade_no'     => $_POST['trade_no'],
+            'trade_status' => $_POST['trade_status'],
         );
     }
 
@@ -84,18 +88,20 @@ class Service implements IThirdPartyPay
      *
      * @return array|bool
      */
-    public function notifyPayReturn(){
+    public function notifyPayReturn()
+    {
         $notify = new Notify(Config::getConfigArray());
         $result = $notify->verifyReturn();
-        if( !$result ){//验签失败 直接返回
+        if (!$result) {//验签失败 直接返回
             echo "fail";
+
             return false;
         }
         echo "success"; //验证成功就应该通知成功
         return array(
-            'out_trade_no'  => $_GET['out_trade_no'],
-            'trade_no'      => $_GET['trade_no'],
-            'trade_status'  => $_GET['trade_status'],
+            'out_trade_no' => $_GET['out_trade_no'],
+            'trade_no'     => $_GET['trade_no'],
+            'trade_status' => $_GET['trade_status'],
         );
     }
 
@@ -104,17 +110,19 @@ class Service implements IThirdPartyPay
      *
      * @return array
      */
-    public function notifyRefundNotify(){
+    public function notifyRefundNotify()
+    {
         $notify = new Notify(Config::getConfigArray());
         $result = $notify->verifyNotify();
-        if( !$result ){
+        if (!$result) {
             echo "fail";
         }
         echo "success";
+
         return array(
-            'batch_no'      => $_POST['batch_no'],
-            'success_num'   => $_POST['success_num'],
-            'result_details'=> $_POST['result_details'],
+            'batch_no'       => $_POST['batch_no'],
+            'success_num'    => $_POST['success_num'],
+            'result_details' => $_POST['result_details'],
         );
     }
 
@@ -129,19 +137,19 @@ class Service implements IThirdPartyPay
     public function search($trade_no, $thirdparty_trade_no)
     {
         $parameter = array(
-            'service'       => 'single_trade_query',
-            'partner'       => trim(Config::PARTNER),
-            'trade_no'      => $thirdparty_trade_no,
-            'out_trade_no'  => $trade_no,
-            '_input_charset'=> trim(strtolower(Config::INPUT_CHARSET)),
+            'service'        => 'single_trade_query',
+            'partner'        => trim(Config::PARTNER),
+            'trade_no'       => $thirdparty_trade_no,
+            'out_trade_no'   => $trade_no,
+            '_input_charset' => trim(strtolower(Config::INPUT_CHARSET)),
         );
-        $submit = new Submit(Config::getConfigArray());
-        $result = $submit->buildRequestHttp($parameter);
+        $submit    = new Submit(Config::getConfigArray());
+        $result    = $submit->buildRequestHttp($parameter);
 
         //@todo 对于查询到的xml进行解析
         $doc = new \DOMDocument();
         $doc->loadXML($result);
-        if( empty($doc->getElementsByTagName( "alipay" )->item(0)->nodeValue) ){
+        if (empty($doc->getElementsByTagName("alipay")->item(0)->nodeValue)) {
             return false;
         }
     }
@@ -157,14 +165,14 @@ class Service implements IThirdPartyPay
     public function close($trade_no, $thirdparty_trade_no)
     {
         $parameter = array(
-            'service'       => 'close_trade',
-            'partner'       => trim(Config::PARTNER),
-            'trade_no'      => $thirdparty_trade_no,
-            'out_trade_no'  => $trade_no,
-            '_input_charset'=> trim(strtolower(Config::INPUT_CHARSET)),
+            'service'        => 'close_trade',
+            'partner'        => trim(Config::PARTNER),
+            'trade_no'       => $thirdparty_trade_no,
+            'out_trade_no'   => $trade_no,
+            '_input_charset' => trim(strtolower(Config::INPUT_CHARSET)),
         );
-        $submit = new Submit(Config::getConfigArray());
-        $result = $submit->buildRequestHttp($parameter);
+        $submit    = new Submit(Config::getConfigArray());
+        $result    = $submit->buildRequestHttp($parameter);
 
         //@todo 根据文档编写返回数据的处理逻辑
     }
@@ -182,25 +190,27 @@ class Service implements IThirdPartyPay
     public function refund($batch_no, $trade_no, $amount, $extra = null)
     {
         $parameter = array(
-            'service'       => 'refund_fastpay_by_platform_pwd',
-            'partner'       => trim(Config::PARTNER),
-            'notify_url'    => Config::NOTIFY_URL,
-            'seller_email'  => Config::SELLER_EMAIL,
-            'refund_date'   => date('Y-m-d H:i:s'),
-            'batch_no'      => $batch_no,
-            'batch_num'     => 1,
-            'detail_data'   => $extra['detail_data'] ? $extra['detail_data'] : "{$trade_no}^{$amount}^支付宝订单协商退款",
-            '_input_charset'=> trim(strtolower(Config::INPUT_CHARSET)),
+            'service'        => 'refund_fastpay_by_platform_pwd',
+            'partner'        => trim(Config::PARTNER),
+            'notify_url'     => Config::NOTIFY_URL,
+            'seller_email'   => Config::SELLER_EMAIL,
+            'refund_date'    => date('Y-m-d H:i:s'),
+            'batch_no'       => $batch_no,
+            'batch_num'      => 1,
+            'detail_data'    => $extra['detail_data'] ? $extra['detail_data'] : "{$trade_no}^{$amount}^支付宝订单协商退款",
+            '_input_charset' => trim(strtolower(Config::INPUT_CHARSET)),
         );
-        $submit = new Submit(Config::getConfigArray());
-        $result = $submit->buildRequestForm($parameter, "get", "确认");
+        $submit    = new Submit(Config::getConfigArray());
+        $result    = $submit->buildRequestForm($parameter, "get", "确认");
+
         return $result;
     }
 
     /**
      * 支付宝的批量退款实现
      */
-    public function refundMany(){
+    public function refundMany()
+    {
         //@todo: 支付宝支付批量退款接口，在此进行实现
     }
 }
